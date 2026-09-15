bd0a670dded44165bedf269802a3d7d6157f93e5
DigitalHouses <digitalhouseskz@gmail.com>
2026-09-16 02:42:30 +0500

    test(dh-pve): include policy config event in canonical UPS event contract

--- dh_pve_app/tests/test_canonical_ups_discovery.py
+++ dh_pve_app/tests/test_canonical_ups_discovery.py
@@ -143,6 +143,7 @@ def test_ups_problem_aggregate_and_native_event_are_canonical():
         "problem_started",
         "problem_recovered",
         "problem_updated",
+        "config_changed",
     ]
     assert event["entity_category"] == "diagnostic"
     assert "json_attributes_topic" not in event
