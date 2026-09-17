476f521062ff956854cf2080be4df4200ffb553c
DigitalHouses <digitalhouseskz@gmail.com>
2026-09-18 04:50:53 +0500

    test(dh-pve): require uninstall to remove root guide

--- dh_pve_app/tests/test_uninstall_contract.py
+++ dh_pve_app/tests/test_uninstall_contract.py
@@ -93,3 +93,14 @@ def test_uninstaller_uses_installed_python_cleanup_and_never_owns_nut_or_depende
         "mosquitto",
     ):
         assert forbidden not in lowered
+
+
+def test_uninstaller_removes_root_operational_guide_only_after_mqtt_cleanup():
+    text = _text()
+
+    assert 'ROOT_GUIDE="/root/dh_app_pve.txt"' in text
+    assert 'rm -f -- "${ROOT_GUIDE}"' in text
+
+    cleanup_pos = text.index("--uninstall-mqtt-cleanup")
+    guide_remove_pos = text.index('rm -f -- "${ROOT_GUIDE}"')
+    assert cleanup_pos < guide_remove_pos
