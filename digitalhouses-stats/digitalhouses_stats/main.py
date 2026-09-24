ebbc6c92c01665ff2612c01bae3620c77d211508
DigitalHouses <digitalhouseskz@gmail.com>
2026-09-24 16:19:32 +0500

    release(stats): report server version 0.4.0

--- digitalhouses-stats/digitalhouses_stats/main.py
+++ digitalhouses-stats/digitalhouses_stats/main.py
@@ -19,7 +19,7 @@ logging.basicConfig(level=settings.log_level.upper())
 
 app = FastAPI(
     title="DigitalHouses Stats",
-    version="0.3.1",
+    version="0.4.0",
     docs_url=None,
     redoc_url=None,
 )
