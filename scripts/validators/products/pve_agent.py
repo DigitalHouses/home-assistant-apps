818d90495825b3c2ae5a41cc69e391d7099a9ff1
DigitalHouses <digitalhouseskz@gmail.com>
2026-09-26 02:56:17 +0500

    release(pve): update validator to 0.5.24

--- scripts/validators/products/pve_agent.py
+++ scripts/validators/products/pve_agent.py
@@ -5,7 +5,7 @@ from typing import Any
 
 from validators.common import fail, require_files
 
-EXPECTED_VERSION = "0.5.23"
+EXPECTED_VERSION = "0.5.24"
 EXPECTED_TOPIC_PREFIX = "DigitalHouses/Global/dh_pve_app"
 EXPECTED_DEVICE_NAME = "DH PVE"
 EXPECTED_REFRESH_ENTITY = "button.dh_app_pve_refresh"
